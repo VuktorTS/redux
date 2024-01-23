66386f0ec8f2c431d81f560d9a395e845a6a996d
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-01-23 14:49:11 +0200

    reinitial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { AppBar } from "components/AppBar/AppBar";
 import { TaskForm } from "components/TaskForm/TaskForm";
 import { TaskList } from "components/TaskList/TaskList";
 
+
 export const App = () => {
   return (
     <Layout>
