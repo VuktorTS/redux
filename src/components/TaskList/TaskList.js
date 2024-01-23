17ee97021761d533e0a2c03a359043f873a2230d
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-01-24 00:39:38 +0200

    add reducer and store

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,28 @@
-import { Task } from "components/Task/Task";
-import css from "./TaskList.module.css";
+import { Task } from 'components/Task/Task';
+import css from './TaskList.module.css';
+import { statusFilters } from '../../redux/constants';
+import { useSelector } from 'react-redux';
+import { getStatusFilter } from '../../redux/selectors';
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 export const TaskList = () => {
+  const tasks = useSelector(state => state.tasks);
+  const statusFilter = useSelector(getStatusFilter);
+  const visebleTasks = getVisibleTasks(tasks, statusFilter);
+
   return (
     <ul className={css.list}>
-      {[].map(task => (
+      {visebleTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
