692966c5b782a09828583200835bca9e169d6c78
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-01-31 11:53:02 +0200

    end

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,24 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducer';
+
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     filters: filtersReducer,
+//   },
+// });
 import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './reducer';
+
+//=============== Before ========================
+// import { tasksReducer, filtersReducer } from './reducer';
+
+//=============== After ========================
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
 });
